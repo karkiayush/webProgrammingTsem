64f8fea22524fa6c0ea56637b6ee92c92124189c
karkiayush <karkiayush619@gmail.com>
2022-12-17 18:59:09 +0545

    props, propsTypes, react componnent and some other topics is learned

--- ReactJs/my-app/src/App.js
+++ ReactJs/my-app/src/App.js
@@ -1,5 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
 let name = "Aayush Vai";
 
@@ -11,41 +12,9 @@ function App() {
     // jsx fragment wraps all of the multiple returns 
     <>
       {/* Navbar  */}
-      <nav className="navbar navbar-expand-lg bg-light">
-        {/* Main div */}
-        <div
-          // Here we'll be using react router, so we replace href="#" by / </nav>
-
-          className="container-fluid">
-          <a className="navbar-brand" href="/">Aayush's Navbar</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-
-              {/* Home link  */}
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">Home</a>
-              </li>
-
-              {/* About link  */}
-              <li className="nav-item">
-                <a className="nav-link" href="/">About</a>
-              </li>
-
-            </ul>
-
-            {/* Nav bar ends  */}
-            <form className="d-flex" role="search">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button className="btn btn-outline-success" type="submit">Search</button>
-            </form>
-          </div>
-        </div>
-      </nav>
-
-      {/* Named export & Default export in JS */}
+      {<Navbar title=
+        "Navbar Title" aboutText="About me"
+        landingpage="Home" />}
     </>
   );
 }
