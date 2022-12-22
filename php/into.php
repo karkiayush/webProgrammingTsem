5d19c7c1aedffcb77bd0f73359dad39f831e604e
karkiayush <karkiayush619@gmail.com>
2022-12-22 09:06:35 +0545

    some of the setup of php enviroment is done & some of the php info is noted

---
+++ php/into.php
@@ -0,0 +1,53 @@
+<!-- asp = active server pages 
+server side jsp : java server page
+firstly developed as the personal home page 
+developer : rasmus lerfdorf in 1994
+
+wamp: for package in windows 
+127.0.0.1 : loop back address i.e accesing the local host using ip address 
+
+            WORKING OF WEBSERVER AND DATA DISPLAY 
+webserver gives the file to php processor and then the php processor  after converting it to html, gives to the web client and data are dispalyed in the browser
+
+
+php process works in 2 mode:
+copy mode: if the php processor gets the plain html file, it directly copy the file and return to the webclient
+
+preprocess mode: whenever the php process gets the php coded file than before returning to the webclient, it process the file and convert to its equivalent html file.
+
+                             PHP KEYWORDS 
+
+and default false if or break do for include require true case else foreach list return var class elseif continue extends global switch while this xor not virtual new 
+
+
+DATA TYPES 
+primitive 
+a. integer 
+b. double
+c. string 
+d. boolean 
+
+
+     DERIVED/COMPOUND 
+arrays 
+object 
+
+
+   ARITHMETIC OPERATIONS 
++ - * / % ++ -- 
+
+    MATHEMATICAL FUNCTIONS 
+floor(x)
+ceil(x)
+round(x)
+srand(x)
+abs(x)
+min(x1,x2,x3,....) 
+max(x1,x2,x3,....)
+sin(x)
+cos(x)
+tan(x)
+asin(x)  log(x)
+acos(x)  exp(x)
+atanx(x) log10(x)   etc...............
+-->
