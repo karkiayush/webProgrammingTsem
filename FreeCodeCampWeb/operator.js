7047152faff292add8dbf3b50eb887bf7dd09f4f
karkiayush <karkiayush619@gmail.com>
2023-01-16 12:27:31 +0545

    operaotr and variables and data types is covered

--- FreeCodeCampWeb/operator.js
+++ FreeCodeCampWeb/operator.js
@@ -6,3 +6,10 @@ console.log(Number(stringValue) === numberValue);
 /*This below Number method convert the other data type to number only for that run time(say single time)*/
 Number(stringValue);
 console.log(stringValue === numberValue);
+
+// Ternary operator 
+const result1 = '55';
+const result2 = 55;
+console.log(result1 == result2 ? 'value is same' : 'value is not same');
+console.log('\n2nd result');
+console.log(result1 == result2 ? 'value is same' : 'value is not same');
